8a87864830179b7e60c507e43b058dd9c609191c
nbijan <144906880+nbijan@users.noreply.github.com>
2023-11-07 14:32:13 +0100

    fix the save first number & operator functionality

--- script.js
+++ script.js
@@ -1,31 +1,71 @@
+const allButtonsContainer = document.querySelectorAll(".btn");
+const calcDisplay = document.querySelector(".cal-screen");
+calcDisplay.textContent = null;
+let isOperatorClicked = false;
+
+//display the input of the user on the calculator display
+allButtonsContainer.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    calcDisplay.textContent += `${e.target.textContent}`;
+    return (displayValue = calcDisplay.textContent);
+  });
+});
+
+//clear the content of the calculator display
+const clearButton = document.querySelector(".btn-clear");
+clearButton.addEventListener("click", (e) => (calcDisplay.textContent = ""));
+
+// saves the operator sign
+const operators = document.querySelectorAll(".operator");
+operators.forEach((button) =>
+  button.addEventListener("click", (event) => {
+    let firstNumberArray = displayValue.split(/[+\-*\/]/);
+    let firstNumber = firstNumberArray[0];
+    calcDisplay.textContent = "";
+    let operator = event.target.textContent;
+    console.log(
+      `the first number is ${firstNumber} and the operator is ${operator}`
+    );
+  })
+);
+
+//Execute operate function when equalSign is clicked.
+const equalSign = document.querySelector(".equal");
+equalSign.addEventListener("click", operate);
+
+// main operations
 function add(firstNumber, secondNumber) {
-  // return +firstNumber + +secondNumber;
-  console.log(+firstNumber + +secondNumber);
+  const addition = +firstNumber + +secondNumber;
+  calcDisplay.textContent = addition;
+  return addition;
 }
 function subtract(firstNumber, secondNumber) {
-  // return firstNumber - secondNumber;
-  console.log(firstNumber - secondNumber);
+  const subtraction = firstNumber - secondNumber;
+  calcDisplay.textContent = subtraction;
+  return subtraction;
 }
 
 function multiply(firstNumber, secondNumber) {
-  // return firstNumber * secondNumber;
-  console.log(firstNumber * secondNumber);
+  const multiplication = firstNumber * secondNumber;
+  calcDisplay.textContent = multiplication;
+  return multiplication;
 }
 
 function divide(firstNumber, secondNumber) {
-  // return firstNumber / secondNumber;
-  console.log(firstNumber / secondNumber);
+  const division = firstNumber / secondNumber;
+  calcDisplay.textContent = division;
+  return division;
 }
 
-let firstNumber = prompt('Enter the first number');
-let operator = prompt('Enter the operator');
-let secondNumber = prompt('Enter the second number');
-
-function operate(firstNumber, operator,secondNumber) {
-    if (operator === '+') add(firstNumber,secondNumber);
-    if (operator === '-') subtract(firstNumber, secondNumber);
-    if (operator === '*') multiply(firstNumber, secondNumber);
-    if (operator === '/') divide(firstNumber, secondNumber);
+function operate(firstNumber, operator, secondNumber) {
+  let displayArray = displayValue.split(/[+\-*\/=]/);
+  displayArray.splice(displayArray.length - 1, 1);
+  firstNumber = displayArray[0];
+  secondNumber = displayArray[1];
+  let match = displayValue.match(/[+\-*\/]/);
+  operator = match[0];
+  if (operator === "+") add(firstNumber, secondNumber);
+  if (operator === "-") subtract(firstNumber, secondNumber);
+  if (operator === "*") multiply(firstNumber, secondNumber);
+  if (operator === "/") divide(firstNumber, secondNumber);
 }
-
-operate(firstNumber, operator,secondNumber);
